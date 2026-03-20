fb0330465965046a00e38325b29a1b9967e9ae5a
Elverton <elvertoncf@gmail.com>
2026-03-20 10:40:36 -0300

    Updating main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int mat[100][100];
+    int mat[n][n];
     for (int i = 0; i < n; i++)
         for (int j = 0; j <= i; j++)
             scanf("%d", &mat[i][j]);
